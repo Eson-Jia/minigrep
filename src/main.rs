339edca28fe040a6ba543ab53457ed8406e4317c
jiayuxin <13520723393@163.com>
2020-10-20 18:08:04 +0800

    错误信息输出到错误输出上

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use minigrep::{Config};
 fn main() {
     let args:Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("{}",err);
+        eprintln!("Problem parsing arguments: {}",err);
         process::exit(1);
     });
     if let Err(err) = minigrep::run(config){
-        println!("got error:{}",err);
+        eprintln!("Applications error: {}",err);
+        process::exit(1);
     };
 }
