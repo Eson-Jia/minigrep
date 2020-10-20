ed772697b90a63a4d147342bff336b6fb63f130d
jiayuxin <13520723393@163.com>
2020-10-20 18:07:16 +0800

    添加大小写不敏感的实现

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 pub struct Config{
     pub file_name: String,
     pub query: String,
+    pub case_insensitive: bool,
 }
 
 impl Config{
@@ -10,16 +12,23 @@ impl Config{
         if args.len()<3{
             return Err("wrong arguments numbers");
         }
+        let case_insensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config {
-            file_name: args[1].clone(),
-            query: args[2].clone(),
+            query: args[1].clone(),
+            file_name: args[2].clone(),
+            case_insensitive,
         })
     }
 }
 
 pub fn run(config:Config)->Result<(),Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_name)?;
-    for line in  search(&config.query,&contents){
+    let results = if config.case_insensitive{
+        search_case_insensitive(&config.query,&contents)
+    }else{
+        search(&config.query,&contents)
+    };
+    for line in  results{
         println!("{}",line);
     }
     Ok(())
@@ -34,6 +43,16 @@ pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
     }
     result
 }
+pub fn search_case_insensitive<'a>(query:&str,contents:&'a str)-> Vec<&'a str>{
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&query){
+            results.push(line);
+        }
+    }
+    results
+}
 
 #[cfg(test)]
 mod tests{
@@ -46,6 +65,16 @@ mod tests{
         product,fast,reliable
         friend
         ";
-        assert_eq!(search(query,contents),vec!["product,fast,reliable"]);
+        assert_eq!(search_case_insensitive(query,contents),vec!["product,fast,reliable"]);
+    }
+    #[test]
+    fn  case_insensitive(){
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        assert_eq!(vec!["Rust:","Truest me."],search(query,contents));
     }
 }
