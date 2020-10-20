9ac7fc6c32d540124bfacac38f6caecd4940f234
jiayuxin <13520723393@163.com>
2020-10-20 16:12:04 +0800

    添加单元测试

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::error::Error;
-
+use std::assert_eq;
 pub struct Config{
     pub file_name: String,
     pub query: String,
@@ -22,4 +22,29 @@ pub fn run(config:Config)->Result<(),Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_name)?;
     println!("contents:{}",contents);
     Ok(())
+}
+
+pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
+    let mut result = Vec::new();
+    for line in contents.lines(){
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn one_result(){
+        let query  = "uct";
+        let contents = "\
+        product,fast,reliable
+        friend
+        ";
+        assert_eq!(search(query,contents),vec!["product,fast,reliable"]);
+    }
 }
