795643524fc79eac0887cea665116a488fa62fda
jiayuxin <13520723393@163.com>
2020-10-22 11:17:06 +0800

    添加单元测试

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ use std::env;
 use std::process;
 use minigrep::{Config};
 
+fn add_one(origin:u32)->u32{
+      origin+1
+}
+
+
+#[cfg(test)]
+mod tests{
+    use crate::add_one;
+
+    #[test]
+    fn test_add_one(){
+        assert_eq!(add_one(100),101);
+    }
+}
+
+
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err|{
         eprintln!("Problem parsing arguments: {}",err);
