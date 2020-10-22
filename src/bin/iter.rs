ad581fed2f3154f492f52cf93992907e6edc874b
jiayuxin <13520723393@163.com>
2020-10-22 11:14:37 +0800

    修改代码

--- src/bin/iter.rs
+++ src/bin/iter.rs
@@ -11,9 +11,10 @@ fn main() {
         // 注意 * 用来解引用，就是直接修改被引用的值
         println!("after inter_mut:{}",int);
     }
-    let mut the_strings = vec![1.to_string(),2.to_string(),3.to_string()];
+    let the_strings = vec![1.to_string(),2.to_string(),3.to_string()];
     for string in the_strings.into_iter(){
         println!("the string:{}",string);
     }
-    println!("the_strings:{:?}",the_strings);
+    // 在这里 the_strings 已经 moved 而不能使用了
+    // println!("the_strings:{:?}",the_strings);
 }
